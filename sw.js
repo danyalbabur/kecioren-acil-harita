10180b3e32234e1a9936f2fcc7ef2164dad1ad4f
Danyal Babur <118133023+danyalbabur@users.noreply.github.com>
2026-03-17 23:20:13 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'acil-harita-v1.2'; // Versiyonu artırarak temizlik yapıyoruz
+const CACHE_NAME = 'ankara-acil-v2.0';
 const ASSETS = [
+    './',
     'index.html',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
     'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
@@ -7,6 +8,7 @@ const ASSETS = [
     'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css',
     'https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js'
 ];
+
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
