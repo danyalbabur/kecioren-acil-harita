4460f88577e6f9cbd0c37b1fa5166b1c6a8ab6bf
Danyal Babur <118133023+danyalbabur@users.noreply.github.com>
2026-03-17 22:52:47 +0300

    Add files via upload

---
+++ nw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'acil-harita-v1.1'; // Versiyonu 1.1 yaptık ki eski bozuk hafızayı silsin
+const ASSETS = [
+    './',
+    './index.html',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
